a0e3e3633592b3ee23fdaa78259f3420596c036f
RPA-java-developer <rodrigo_piragauta@hotmail.com>
2026-06-13 23:27:29 -0500

    Commit inicial - Creación proyecto FranquiciaRPA

---
+++ src/main/java/com/api/rest/reactiva/franquiciaRPA/FranquiciaRpaApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.reactiva.franquiciaRPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciaRpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciaRpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/reactiva/franquiciaRPA/FranquiciaRpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.reactiva.franquiciaRPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciaRpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
